b35c4ca7e414c080d7804cb2236d0f8415cd96f1
Bonom0 <caioazbonomo@gmail.com>
2025-03-31 17:13:53 -0300

    Importado os componentes criados específicos para a tela Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,26 @@
 import React from "react";
-
+import { FaGithub, FaPlus } from 'react-icons/fa';
 import { 
-  Title 
+  Container,
+  Form,
+  SubmitButton
 } from './styles';
 
 export default function Main(){
   return(
-    <Title>Main</Title>
+    <Container>
+      <h1>
+        <FaGithub size={25}/>
+        Meus Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositórios"/>
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14}/>
+        </SubmitButton>
+      </Form>
+    </Container>
   )
 }
