7be0c0b388ec1d70385a3ca49cc144be64c93e5c
Bonom0 <caioazbonomo@gmail.com>
2025-04-03 16:51:46 -0300

    Criado useEffect para realizar requisições na api e trazer os dados do repositório

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Container } from "./styles";
+import api from "../../services/api";
 
 export default function Repositorio({match}){
   const { repositorio } = useParams();
+  
+  const [ repositorioSelecionado, setRepositorioSelecionado ] = useState({});
+  const [ issues, setIssues ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    async function load(){
+      const nomeRepo = decodeURIComponent(repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+
+      setRepositorioSelecionado(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+
+    }
+
+    load();
+  }, [repositorio])
+  
   return(
-    <h1 style={{color:'#FFF'}}>
-      Repositorio:
-      {decodeURIComponent(repositorio)}  
-    </h1>
+    <Container>
+
+    </Container>
     
   )
 }
