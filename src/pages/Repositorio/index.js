dad12f813fdd7ba077d3c8014657d4a70a0cc6aa
caio <caioazbonomo@gmail.com>
2025-04-03 20:17:09 -0300

    Adicionado componente de paginação e criado função para a paginação funcionar

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+} from "./styles";
 import { FaArrowLeft } from "react-icons/fa";
 import api from "../../services/api";
 
@@ -10,6 +17,7 @@ export default function Repositorio({ match }) {
   const [repositorioSelecionado, setRepositorioSelecionado] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function load() {
@@ -32,6 +40,28 @@ export default function Repositorio({ match }) {
 
     load();
   }, [repositorio]);
+
+  useEffect(() => {
+    async function loadIssue() {
+      const nomeRepo = decodeURIComponent(repositorio);
+
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params: {
+          state: "open",
+          page,
+          per_page: 5,
+        },
+      });
+
+      setIssues(response.data);
+    }
+
+    loadIssue();
+  }, [repositorio, page]);
+
+  function handlePage(action) {
+    setPage(action === "back" ? page - 1 : page + 1);
+  }
 
   if (loading) {
     return (
@@ -74,6 +104,24 @@ export default function Repositorio({ match }) {
           </li>
         ))}
       </IssuesList>
+
+      <PageActions>
+        <button
+          type="button"
+          onClick={() => handlePage("back")}
+          disabled={page < 2}
+        >
+          Voltar
+        </button>
+
+        <div>
+          <p>{page}</p>
+        </div>
+
+        <button type="button" onClick={() => handlePage("next")}>
+          Próximo
+        </button>
+      </PageActions>
     </Container>
   );
 }
